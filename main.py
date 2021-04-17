f7db62e847c4d71bf217e7a5f3a21c6bff27f676
Hamisi-Esq <brivahamisi@gmail.com>
2021-04-18 00:38:55 +0300

    updated

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ print("Hello World")
 # name = input("Input the ame of the Student")
 # print("The name of the Student is", name)
 
-a = int(input("Input the First Number"))
-b = int(input("Input the Second number"))
-print(a+b)
+# a = int(input("Input the First Number"))
+# b = int(input("Input the Second number"))
+# print(a+b)
+
+L1= ["John",102,"USA"]
+L2 = [1,2,3,4,5,6]
+
+print(type(L1))
+print(type(L2))
+
+tup =("apple","mango","orange","banana")
+print(type(tup))
+print(tup)
