c385d36d0a6b8effe52ac118dd8a7de833e8dd08
Hamisi-Esq <brivahamisi@gmail.com>
2021-04-18 00:13:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello World")
+
+# name = input("Input the ame of the Student")
+# print("The name of the Student is", name)
+
+a = int(input("Input the First Number"))
+b = int(input("Input the Second number"))
+print(a+b)
